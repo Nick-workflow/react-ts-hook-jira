83efd41e55d1f838f8c1d000c323f9179bc10ef3
杨涛 <yangtaoeng@163.com>
2021-10-20 03:00:04 +0800

    style: 用 Row 布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,19 +1,48 @@
 /*
  * @Author: YangTao(Niklaus)
  * @LastEditors: YangTao(Niklaus)
- * @LastEditTime: 2021-10-16 14:33:42
+ * @LastEditTime: 2021-10-20 02:54:14
  * @Description: file content
  */
 
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-contex";
 import { ProjectListScreen } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Header = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
